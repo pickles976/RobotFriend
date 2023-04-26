745560f8051ad3d5d286387f494c86cfaf0f7102
pickles976 <sebaslogo@gmail.com>
2023-04-26 11:12:36 -0500

    Added RPIO

--- src/controller/src/driver.py
+++ src/controller/src/driver.py
@@ -1,27 +1,20 @@
-#!/usr/bin/env python
-import RPi.GPIO as GPIO
-# from time import sleep
+from RPIO import PWM
+from time import sleep
 
-pwmpin = 12				# PWM pin connected to LED
-GPIO.setwarnings(False)			#disable warnings
-GPIO.setmode(GPIO.BOARD)		#set pin numbering system
-GPIO.setup(pwmpin,GPIO.OUT)
-pi_pwm = GPIO.PWM(pwmpin,50)		#create PWM instance with frequency
-pi_pwm.start(0)				#start PWM of required Duty Cycle 
+servo = PWM.Servo()
 
-# duty cycle range
-# 5 -> 7.5 -> 10
-# 5 is reverse, 7.5 is stopped, 10 is forward
+# 1000 -> 1500 -> 2000
 
-pi_pwm.ChangeDutyCycle(10)
+# Set servo on GPIO32 to 2000µs (2ms)
+servo.set_servo(32, 2000)
 
-# while True:
-#     for duty in range(0,101,1):
-#         pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
-#         sleep(0.01)
-#     sleep(0.5)
-    
-#     for duty in range(100,-1,-1):
-#         pi_pwm.ChangeDutyCycle(duty)
-#         sleep(0.01)
-#     sleep(0.5)
+# Set servo on GPIO17 to 2000µs (2.0ms)
+servo.set_servo(17, 2000)
+
+while True:
+    for i in range(0,3):
+        servo.set_servo(32, 1000 + (i * 500))
+        sleep(5)
+
+# Clear servo on GPIO17
+servo.stop_servo(17)
