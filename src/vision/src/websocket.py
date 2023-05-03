5a66a730731b4b25e1297954b2f67e0ad4a81d36
pickles976 <sebaslogo@gmail.com>
2023-05-02 19:27:56 -0500

    add websocket server

---
+++ src/vision/src/websocket.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import asyncio
+import websockets
+import cv2
+import time
+
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
+SCALE = 0.7
+FPS = 24.0
+
+print("Initializing webcam...")
+imgCap = cv2.VideoCapture(0)
+print("Webcam initialized!")
+
+# create handler for each connection
+async def handler(websocket, path):
+
+    time_elapsed = 0
+    prev = 0
+
+    while True:
+
+        time_elapsed = time.time() - prev
+
+        if time_elapsed > 1.0/FPS:
+
+            # Capture the video frame
+            ret, frame = imgCap.read()
+
+            # Resize
+            width = int(frame.shape[1] * SCALE)
+            height = int(frame.shape[0] * SCALE)
+            frame = cv2.resize(frame, (width, height))
+
+            # To bytes
+            frameData = cv2.imencode('.jpg', frame, encode_param)[1].tobytes()
+        
+            await websocket.send(frameData)
+
+            prev = time.time()
+
+print("Starting server thread...")
+start_server = websockets.serve(handler, "", 8000)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
