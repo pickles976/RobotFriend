02eab7c76930e3f7b27e424ef601439ca60281cf
pickles976 <sebaslogo@gmail.com>
2023-05-03 12:06:40 -0500

    Added better image capture script

--- src/robot/src/vision.py
+++ src/robot/src/vision.py
@@ -6,14 +6,15 @@
 import io
 import time
 import picamera
+from picamera.array import PiRGBArray
 from sensor_msgs.msg import Image
 import rospy
-import numpy as np
 import ros_numpy
+import signal
 
 # (640, 480)
 # (1280, 960)
-WIDTH, HEIGHT = 320, 240
+WIDTH, HEIGHT = 640, 480
 camera=None
 topic = 'picamera/image'
 node_name = 'camera'
@@ -21,39 +22,41 @@ node_name = 'camera'
 def talker():
 
     print('Initializing node: {} with topic "{}"'.format(node_name, topic))
-    pub = rospy.Publisher(topic, Image, queue_size=10)
+    pub = rospy.Publisher(topic, Image, queue_size=1)
     rospy.init_node(node_name, anonymous=True)
-    rate = rospy.Rate(10) # 10 hz
+    rate = rospy.Rate(30) # 10 hz
 
     print("Starting camera...")
     camera = picamera.PiCamera()
     camera.resolution = (WIDTH, HEIGHT)
-    camera.framerate = 10
+    rawCapture = PiRGBArray(camera, size=(WIDTH, HEIGHT))
+    camera.framerate = 30
     camera.rotation = 180
     camera.awb_mode = 'off'
     camera.awb_gains = (1.4, 1.5)
     
     time.sleep(2)
 
-
     print("Starting capture...")
-    while not rospy.is_shutdown():
-
-        message = Image()
-
-        output = np.empty((HEIGHT*WIDTH*3,),dtype=np.uint8)
-        camera.capture(output, 'rgb')
-        output = output.reshape((HEIGHT,WIDTH,3))
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        output = frame.array
 
         message = ros_numpy.msgify(Image, output, encoding='rgb8')
 
-        # rospy.loginfo(message)
-        print("Sending image...")
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+        print("Sending image...  (Press CTRL+C to exit)")
         pub.publish(message)
         rate.sleep()
 
+def handler(signum, frame):
+    print("CTRL+C pressed, exiting gracefully...")
+    camera.close()
+
+
 if __name__ == '__main__':
     try:
+        signal.signal(signal.SIGINT, handler)
         talker()
     except rospy.ROSInterruptException:
         pass
