f091fa8f7810c4a86a4921c67efb02797fdd2d44
pickles976 <sebaslogo@gmail.com>
2023-05-05 13:12:39 -0500

    Downscaled resolution to 1280x960

--- src/vision/src/snap_pic.py
+++ src/vision/src/snap_pic.py
@@ -2,7 +2,7 @@ from picamera import PiCamera
 import time
 from time import sleep
 
-WIDTH, HEIGHT = 1920, 1080
+WIDTH, HEIGHT = 1280, 960
 
 camera = PiCamera()
 camera.resolution = (WIDTH, HEIGHT)
