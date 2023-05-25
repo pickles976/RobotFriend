15af4666918eb52e0b7e4e391a0a7ce35b7251a8
pickles976 <sebaslogo@gmail.com>
2023-05-25 10:21:02 -0500

    Added saving controls input

--- src/vision/src/record_image_sequence.py
+++ src/vision/src/record_image_sequence.py
@@ -1,18 +1,39 @@
 #!/usr/bin/python
 from picamera import PiCamera
 from time import sleep
+import rospy
+from std_msgs.msg import Twist
+import json
 
-WIDTH, HEIGHT = 1280, 960
+messages = []
 
-camera = PiCamera()
-camera.resolution = (WIDTH, HEIGHT)
-camera.rotation = 180
-camera.framerate = 10
-camera.start_preview()
-sleep(1)
+def callback(data):
+    global messages
+    messages.append(data)
+    
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("velocity_controller/cmd_vel", Twist, callback)
 
-for filename in camera.capture_continuous('img{counter:03d}.jpg',use_video_port=True):
-    print('Captured %s' % filename)
-    sleep(0.1) # wait 0.1s
+if __name__ == '__main__':
 
-camera.stop_preview()
+    listener()
+
+    WIDTH, HEIGHT = 1280, 960
+
+    camera = PiCamera()
+    camera.resolution = (WIDTH, HEIGHT)
+    camera.rotation = 180
+    camera.framerate = 10
+    camera.start_preview()
+    sleep(1)
+
+    try:
+        for filename in camera.capture_continuous('img{counter:03d}.jpg',use_video_port=True):
+            print('Captured %s' % filename)
+            sleep(0.1) # wait 0.1s
+    except KeyboardInterrupt: 
+        camera.stop_preview()
+        
+        with open('controls.json', 'w') as f:
+            json.dump({ "inputs" : messages}, f)
